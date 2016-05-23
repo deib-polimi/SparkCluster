3e9a40864c2deb2612f5049a20abe38b62d51872
pietrodn <powerpdn@gmail.com>
2016-05-23 21:15:56 +0200

    No prompt for installing

--- master/bootstrap.sh
+++ master/bootstrap.sh
@@ -6,13 +6,13 @@ swapon /swapfile
 echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
 
 # update and upgrade
-apt-get update
-apt-get upgrade
+apt-get update -y
+apt-get upgrade -y
 
 # JDK 8
 apt-get install -y python-software-properties debconf-utils
 add-apt-repository -y ppa:webupd8team/java
-apt-get update
+apt-get update -y
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | debconf-set-selections
 apt-get install -y oracle-java8-installer
 apt-get install -y oracle-java8-set-default
@@ -21,7 +21,7 @@ apt-get install -y oracle-java8-set-default
 cd /etc/apt/sources.list.d
 wget http://public-repo-1.hortonworks.com/ambari/ubuntu14/2.x/updates/2.2.2.0/ambari.list
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com B9733A7A07513CAD
-apt-get update
+apt-get update -y
 apt-get install -y ambari-server
 apt-get install -y ambari-agent
 apt-get install -y ntp
