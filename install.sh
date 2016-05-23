455157fde2b72f33303fa44f007e2c382032411b
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-05-23 22:26:55 +0200

    minor fixes

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ cd $DIR
 trap 'cd $OLD_DIR' 0
 
 if [ $# -ne 1 ]; then
-	echo "Usage: $0 {master|slave|performance}"
-	exit 1
+    echo "Usage: $0 {master|slave|performance}"
+    exit 1
 fi
 
 if [ "$MODE" == "master" ]; then
@@ -18,20 +18,20 @@ if [ "$MODE" == "master" ]; then
     sudo master/bootstrap.sh
     git clone http://github.com/carduz/spark-perf.git git/spark-perf
     cp spark-perf/config.py git/spark-perf/config
-	exit 0
+    exit 0
 elif [ "$MODE" == "performance" ]; then
-	echo "master"
-	sudo performance/bootstrap.sh
+    echo "performance"
+    sudo performance/bootstrap.sh
     git clone https://github.com/carduz/spark-log-processor.git git/spark-log-processor
     git/spark-log-processor/build.sh
     cp git/spark-log-processor/sparkloggerparser/target/uber-sparkloggerparser-0.0.1-SNAPSHOT.jar analysis_tool
     cp git/performance-estimator/target/performance-estimator-0.0.1-SNAPSHOT-jar-with-dependencies.jar analysis_tool
-	exit 0
+    exit 0
 elif [ "$MODE" == "slave" ]; then
-	echo "slave"
-	sudo slave/bootstrap.sh
-	exit 0
+    echo "slave"
+    sudo slave/bootstrap.sh
+    exit 0
 else
-	echo "Usage: $0 {master|slave|performance}"
-	exit 1
+    echo "Usage: $0 {master|slave|performance}"
+    exit 1
 fi
