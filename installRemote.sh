5daef8aa72ca8bd66f09ec7cb5eb0aac901b2ba0
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-05-24 11:37:31 +0200

    draft install remote

---
+++ installRemote.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+REMOTE=$1
+MODE=$2
+
+OLD_DIR=${PWD}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+trap 'cd $OLD_DIR' 0
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 {remote address} {master|slave|performance}"
+    exit 1
+fi
+
+#copy installer
+scp -r ../SparkCluster $REMOTE:~/SparkCluster
+
+#install
+ssh $REMOTE "cd ~/SparkCluster/install.sh $MODE"
